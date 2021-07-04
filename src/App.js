7084c0c48bdf9ab02cacc3d35cfd284c297bc09b
ibrahemomari <ibrahem.omari96@gmail.com>
2021-07-04 18:46:09 +0300

    lab 11 front

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+import { withAuth0 } from '@auth0/auth0-react';
+import BestBooks from './BestBooks';
+import Login from './Login'
+import Profile from './Profile';
 import Footer from './Footer';
+import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+
 
 class App extends React.Component {
 
@@ -15,20 +20,25 @@ class App extends React.Component {
     return(
       <>
         <Router>
-          <IsLoadingAndError>
             <Header />
             <Switch>
               <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */
+                  this.props.isAuthenticated ?<BestBooks/>:<Login/>
+
+                }
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route path="/profile">
+                {this.props.auth0.isAuthenticated && <Profile/> }
+              </Route>
             </Switch>
             <Footer />
-          </IsLoadingAndError>
+
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default withAuth0(App);
---
+++ src/Profile.js
@@ -0,0 +1,24 @@
+
+import React, { Component } from 'react'
+import { withAuth0 } from '@auth0/auth0-react';
+
+export class Profile extends Component {
+    render() {
+        return (
+            <div className="userInfo-section">
+                {
+                    this.props.auth0.isAuthenticated&&
+                    <>
+                    <h1 className="username-title">{this.props.auth0.user.name}</h1>
+                    <img className="user-img" src={this.props.auth0.user.picture} alt={this.props.auth0.user.name}/>
+                    <p className="user-email">{this.props.auth0.user.email}</p>
+                    </>
+
+                }
+                
+            </div>
+        )
+    }
+}
+
+export default withAuth0(Profile);
