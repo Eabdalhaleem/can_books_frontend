8603dad2596d56f5cfc20e07ca04da83ead7bf25
ibrahemomari <ibrahem.omari96@gmail.com>
2021-07-05 21:20:01 +0300

    lap12 v2

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import {Card,Jumbotron} from 'react-bootstrap/';
 import './BestBooks.css';
 import { withAuth0 } from '@auth0/auth0-react'
 import axios from 'axios'
@@ -11,46 +11,48 @@ class MyFavoriteBooks extends React.Component {
       super(props);
       this.state={
         myBook:[],
-
-
       }
     }
 
-    componentDidMount = () => {
-    if (this.props.auth0.isAuthenticated) {
-      this.props.auth0.getIdTokenClaims().then(res => {
-        const jwt = res.__raw;
-        const config = {
-          headers: { "Authorization":`Bearer ${ jwt }`
-        },
-        method: 'get',
-        baseURL: process.env.REACT_APP_AUTH0_DOMAIN,
-        url: '/authorize'
-      }
-       axios(config)
-        .then(axiosResults => console.log(axiosResults.data))
-        .catch(err => console.error(err));
-     }).catch(err => console.error(err));
-   }
+
+componentDidMount(){
+  this.getBooks();
 }
 
 getBooks=async ()=>{
+  console.log('test');
   const {user}=this.props.auth0;
   const urlBooks =`${process.env.REACT_APP_BACKEND_URL}/books?email=${user.email}`;
-  const reqBook= await axios.get(urlBooks)
-  
+  const reqBook= await axios.get(urlBooks);
+  console.log(reqBook.data.books);
+  this.setState({
+    myBook:reqBook.data.books
+  })
+  console.log(this.state.myBook);
 }
 
 render() {
   return (
-    <Jumbotron>
-      <h1>My Favorite Books</h1>
-      <p>
-        This is a collection of my favorite books
-      </p>
-    </Jumbotron>
+    <>
+      <Jumbotron>
+        <h1>My Favorite Books</h1>
+        <p>
+          This is a collection of my favorite books
+        </p>
+      </Jumbotron>
+      {this.state.myBook.map((book, index) =>
+          <>
+          <Card className='cardBook'>
+            <Card.Body key={index}>
+              <Card.Title>Book name: {book.name}</Card.Title>
+              <Card.Text>Description: {book.description}</Card.Text>
+              <Card.Text>Status: {book.status}</Card.Text>
+            </Card.Body>
+            </Card>
+          </>
+          )}
+    </>
   )
-}
-}
+}}
 
 export default withAuth0(MyFavoriteBooks)
